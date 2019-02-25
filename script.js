64ba68f9f4c2d0956c780f09f7ef8dd71882027c
georgy4 <wallzy@mail.ru>
2019-02-25 22:40:19 +0300

    Now this site is adaptive for all screen size

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 var openCart = document.querySelector('.cart');
 var closeBtn = document.querySelector('.close-btn');
 var field = document.querySelector('.close--cart');
+var menuButton = document.querySelector('.menu-btn');
+var mobileMunu = document.querySelector('.menu');
 
 openCart.addEventListener('click', function () {
     field.classList.toggle('open--cart');
@@ -10,5 +12,7 @@ closeBtn.addEventListener('click', function () {
     field.classList.toggle('open--cart');
 });
 
-
+menuButton.addEventListener('click', function () {
+    mobileMunu.classList.toggle('mobile-menu-visible');
+});
 
